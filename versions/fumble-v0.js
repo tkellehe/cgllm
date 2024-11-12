6344a407111efe6e7c476ad2713668a19b8ee41a
tkellehe <mitchell.kelleher@gmail.com>
2024-11-12 09:53:27 -0500

    Update fumble-v0.js

--- versions/fumble-v0.js
+++ versions/fumble-v0.js
@@ -111,6 +111,8 @@
 - \`@0+~ thx\`: Print first argument + \`" thanks"\`.
 - \`#4-(@#)P\`: Subtract argument from 4 and print.
 - \`@+~6\`: Print argument + \`"six"\`.
+- \`x10$FlP\`: Print the first 10 Fibonacci numbers.
+- \`p$lx20\`: Print the first 20 prime numbers.
 `;
 
     // Find all divs with class "fumble-v0"
